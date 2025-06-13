da1d68c52251ef715bdf9bbcec1cb401f8cb9261
pengluxe <penglogisticsltd@gmail.com>
2025-06-13 13:06:45 +0100

    book.js

--- api/book.js
+++ api/book.js
@@ -1,10 +1,18 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.penglogistics.com');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    // Handle preflight
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
 
   const { pickup, dropoff, item_type, note } = req.body;
-
   const apiKey = process.env.DORA_API_KEY;
 
   try {
@@ -33,7 +41,3 @@ export default async function handler(req, res) {
     return res.status(500).json({ message: 'Server error', error: error.message });
   }
 }
-console.log("Sending to Dora:", {
-  pickup, dropoff, item_type, note
-});
-console.log("Dora Response:", data);
